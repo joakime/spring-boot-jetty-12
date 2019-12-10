08e307b91e889eb4cf0a10de769ffe916451faa6
Andy Wilkinson <awilkinson@pivotal.io>
2019-12-10 11:39:03 +0000

    Update CI pipeline to build snapshots with Gradle

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,6 +5,5 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
-run_maven -N clean verify
-run_maven -f spring-boot-project/pom.xml clean deploy -U -Dfull -DaltDeploymentRepository=distribution::default::file://${repository}
+./gradlew --no-daemon -PdeploymentRepository=${repository} clean build publishAllPublicationsToDeploymentRepository
 popd > /dev/null
