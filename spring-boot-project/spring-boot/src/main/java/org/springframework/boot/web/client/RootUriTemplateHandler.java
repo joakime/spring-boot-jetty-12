b07ef914ccbb8013b2f1959926ac487937f5f2d6
Andy Wilkinson <wilkinsona@vmware.com>
2021-03-19 11:58:10 +0000

    Apply any root URI to RestTemplate metric's URI tag

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/client/RootUriTemplateHandler.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/client/RootUriTemplateHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,6 +82,16 @@ public class RootUriTemplateHandler implements UriTemplateHandler {
 
 	public String getRootUri() {
 		return this.rootUri;
+	}
+
+	/**
+	 * The {@link UriTemplateHandler} to which this handler delegates after applying the
+	 * root URI.
+	 * @return delegate handler
+	 * @since 2.3.10
+	 */
+	public UriTemplateHandler getDelegate() {
+		return this.handler;
 	}
 
 	/**
