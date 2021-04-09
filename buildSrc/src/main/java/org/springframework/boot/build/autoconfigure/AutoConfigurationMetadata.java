1d428c1b12856d3765997f5d307475d5de81c6b8
Andy Wilkinson <wilkinsona@vmware.com>
2021-04-09 15:07:51 +0100

    Filter out non-public classes when documenting auto-config classes

--- buildSrc/src/main/java/org/springframework/boot/build/autoconfigure/AutoConfigurationMetadata.java
+++ buildSrc/src/main/java/org/springframework/boot/build/autoconfigure/AutoConfigurationMetadata.java
@@ -17,11 +17,15 @@
 package org.springframework.boot.build.autoconfigure;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
+import java.util.LinkedHashSet;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 import org.gradle.api.DefaultTask;
@@ -30,7 +34,10 @@ import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskAction;
 
+import org.springframework.asm.ClassReader;
+import org.springframework.asm.Opcodes;
 import org.springframework.core.CollectionFactory;
+import org.springframework.util.StringUtils;
 
 /**
  * A {@link Task} for generating metadata describing a project's auto-configuration
@@ -78,10 +85,36 @@ public class AutoConfigurationMetadata extends DefaultTask {
 		Properties autoConfiguration = CollectionFactory.createSortedProperties(true);
 		Properties springFactories = readSpringFactories(
 				new File(this.sourceSet.getOutput().getResourcesDir(), "META-INF/spring.factories"));
-		autoConfiguration.setProperty("autoConfigurationClassNames",
-				springFactories.getProperty("org.springframework.boot.autoconfigure.EnableAutoConfiguration"));
+		String enableAutoConfiguration = springFactories
+				.getProperty("org.springframework.boot.autoconfigure.EnableAutoConfiguration");
+		Set<String> classNames = StringUtils.commaDelimitedListToSet(enableAutoConfiguration);
+		Set<String> publicClassNames = new LinkedHashSet<>();
+		for (String className : classNames) {
+			File classFile = findClassFile(className);
+			if (classFile == null) {
+				throw new IllegalStateException("Auto-configuration class '" + className + "' not found.");
+			}
+			try (InputStream in = new FileInputStream(classFile)) {
+				int access = new ClassReader(in).getAccess();
+				if ((access & Opcodes.ACC_PUBLIC) == Opcodes.ACC_PUBLIC) {
+					publicClassNames.add(className);
+				}
+			}
+		}
+		autoConfiguration.setProperty("autoConfigurationClassNames", String.join(",", publicClassNames));
 		autoConfiguration.setProperty("module", getProject().getName());
 		return autoConfiguration;
+	}
+
+	private File findClassFile(String className) {
+		String classFileName = className.replace(".", "/") + ".class";
+		for (File classesDir : this.sourceSet.getOutput().getClassesDirs()) {
+			File classFile = new File(classesDir, classFileName);
+			if (classFile.isFile()) {
+				return classFile;
+			}
+		}
+		return null;
 	}
 
 	private Properties readSpringFactories(File file) throws IOException {
