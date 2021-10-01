a46a432212e658cefca7068cffa8d8625f66fa2e
Andy Wilkinson <wilkinsona@vmware.com>
2021-10-01 09:46:13 +0100

    Test the CI worker with Carbon Black installed

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -6,8 +6,8 @@ repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
 if [[ -d /opt/openjdk-toolchain ]]; then
-  ./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PdeploymentRepository=${repository} build publishAllPublicationsToDeploymentRepository -PtoolchainVersion=${TOOLCHAIN_JAVA_VERSION} -Porg.gradle.java.installations.auto-detect=false -Porg.gradle.java.installations.auto-download=false -Porg.gradle.java.installations.paths=/opt/openjdk-toolchain/
+  ./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --no-build-cache --max-workers=4 -PdeploymentRepository=${repository} build -PtoolchainVersion=${TOOLCHAIN_JAVA_VERSION} -Porg.gradle.java.installations.auto-detect=false -Porg.gradle.java.installations.auto-download=false -Porg.gradle.java.installations.paths=/opt/openjdk-toolchain/
 else
-  ./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PdeploymentRepository=${repository} build publishAllPublicationsToDeploymentRepository
+  ./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --no-build-cache --max-workers=4 -PdeploymentRepository=${repository} build
 fi
 popd > /dev/null
